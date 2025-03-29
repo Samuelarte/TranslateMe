5dc370b4f319716fad1f79800ab752120cf2ce49
Samuel Lopez <samuelarte35@gmail.com>
2025-03-28 22:44:45 -0500

    Initial Commit

---
+++ TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Erisanny Vargas on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TranslateMeTests/TranslateMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeTests.swift
+//  TranslateMeTests
+//
+//  Created by Erisanny Vargas on 3/28/25.
+//
+
+import Testing
+@testable import TranslateMe
+
+struct TranslateMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
